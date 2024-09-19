184373fa13662ace3e204bfb9f0dbd034fc0f08c
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2024-09-19 11:00:15 +0200

    minor updates
    - added some comments
    - fixed sample skip time
    - improved printfs

--- main.c
+++ main.c
@@ -47,15 +47,30 @@ int main(void)
 
 				if(show_all || raw_packet[2]==0x60)
 				{
+					//get local time
 					time_t now = time(NULL);
 					struct tm* tm_now = localtime(&now);
 
-					printf("[%02d:%02d:%02d] Raw data: ", tm_now->tm_hour, tm_now->tm_min, tm_now->tm_sec);
+					//packet has been detected
+					printf("\033[96m[%02d:%02d:%02d] \033[92mPacket received\033[39m\n", tm_now->tm_hour, tm_now->tm_min, tm_now->tm_sec);
+
+					//print type
+					printf(" ├ \033[93mType:\033[39m ");
+					if(raw_packet[2]==0x60)
+						printf("time\n");
+					else
+						printf("other\n");
+
+					//print raw contents
+					printf(" ├ \033[93mRaw data:\033[39m ");
 					for(uint8_t i=0; i<12; i++)
 						printf("%02X ", raw_packet[i]);
+					printf("\n");
 
+					//descramble contents (raw_packet[] is not raw anymore :)
 					for(uint8_t i=0; i<5; i++) raw_packet[3+i]^=scram[i];
 
+					//extract the 30-bit timestamp into a 4-byte array
 					uint8_t raw_timestamp[4]={	((raw_packet[3]<<1)&0x3F)|(raw_packet[4]>>7),
 												(raw_packet[4]<<1)|(raw_packet[5]>>7),
 												(raw_packet[5]<<1)|(raw_packet[6]>>7),
@@ -65,17 +80,22 @@ int main(void)
 					uint8_t tmp;
 					tmp=raw_timestamp[0]; raw_timestamp[0]=raw_timestamp[3]; raw_timestamp[3]=tmp;
 					tmp=raw_timestamp[1]; raw_timestamp[1]=raw_timestamp[2]; raw_timestamp[2]=tmp;
+
+					//convert the timestamp into seconds since 01-01-2000 (each tick is 3s)
 					*((uint32_t*)raw_timestamp)*=3;
-					printf("Timestamp: %u", *((uint32_t*)raw_timestamp));
+
+					//print the timedtamp
+					printf(" ├ \033[93mTimestamp:\033[39m %u\n", *((uint32_t*)raw_timestamp));
 					time_t eczas = epoch+*((uint32_t*)raw_timestamp);
-					printf(" (%04d-%02d-%02d %02d:%02d:%02d)",
+
+					//print decoded time
+					printf(" ├ \033[93mDecoded:\033[39m %04d-%02d-%02d %02d:%02d:%02d\n",
 						localtime(&eczas)->tm_year+1900,
 						localtime(&eczas)->tm_mon+1,
 						localtime(&eczas)->tm_mday,
 						localtime(&eczas)->tm_hour,
 						localtime(&eczas)->tm_min,
 						localtime(&eczas)->tm_sec);
-					printf("\n");
 				}
 
 				skip_samples=1;
@@ -85,7 +105,7 @@ int main(void)
 		else
 		{
 			skip_cnt++;
-			if(skip_cnt==12*8*10)
+			if(skip_cnt==52*10) //skip 52 samples (3.0-1.92=1.08; 1.08/0.02 is 54, we are adding some margin here)
 			{
 				skip_cnt=0;
 				skip_samples=0;
